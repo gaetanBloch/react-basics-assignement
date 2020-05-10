4b797510309a1452a32d039120490324ffe59bb3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-10 10:47:48 +0200

    Added Stylesheet to style to the UserOutput

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './UserOutput.css'
 
 const UserOutput = (props) => {
   return (
-    <div>
+    <div className="UserOutput">
       <p>My username is: {props.username}</p>
       <p>{props.children}</p>
     </div>
